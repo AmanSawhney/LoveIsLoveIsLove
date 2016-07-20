1f1941d083b7d2cac1e8db8a8ec54ebee10bbbdc
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-20 11:51:43 -0400

    Cleaned up the UI and pulled positive stories from Readit

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,13 +3,15 @@ require 'nokogiri'
 module WelcomeHelper
 	def scrape(text)
 		links = []
-		doc = Nokogiri::HTML(open('https://www.google.com/search?q=' + text))
-
-		doc.css('h3.r a').each do |link|
-			 links.push(link['href'])
-		end 
-		return links[rand(links.size)]
-
+		source = open("https://www.reddit.com/r/UpliftingNews/",    "User-Agent" => "Code-Maven-Example (see: http://code-maven.com/download-a-page-using-ruby )")
+		page = Nokogiri::HTML(source)
+		page.css('a.title').each do |link|
+			if link['href'].include?('http')
+				links << { content: link.content, href: link['href'] }
+			else
+				links << { content: link.content, href: "http://reddit.com" + link['href'] }
+			end
+		end
+		return links[rand(links.size)][:href]
 	end
-
 end
